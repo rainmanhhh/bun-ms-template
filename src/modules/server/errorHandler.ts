b710ca6d674f8b67c83412c3c28ea3951aef3002
rainmanhhh <410488219@qq.com>
2025-06-26 22:23:43 +0800

    feat: use custom api template to fix error handler problem

--- src/modules/server/errorHandler.ts
+++ src/modules/server/errorHandler.ts
@@ -5,7 +5,7 @@ import { server } from './server.ts'
 export default function () {
   server.use((err: Error | null, _req: Request, res: Response, next: NextFunction) => {
     if (err) {
-      logger.error('Unhandled error', err)
+      logger.error('Unhandled error: ', err)
       res.status(500).json({ code: 500, message: err.message })
     } else {
       next()
