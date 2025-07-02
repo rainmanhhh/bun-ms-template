2f9b10b75e092a232cf1084d864808156354c81d
huangwl <410488219@qq.com>
2025-07-02 08:20:11 +0800

    feat: 加入errorHandler演示

--- src/modules/server/controller/FooBarApiImpl.ts
+++ src/modules/server/controller/FooBarApiImpl.ts
@@ -1,16 +1,18 @@
-import type { FooBarApi, GetMySeqResponse } from '../../../generated/server/api/fooBar/types.ts'
+import type { FooBarApi, TestResponse } from '../../../generated/server/api/fooBar/types.ts'
 import { dateUtil } from '../../../util/dateUtil.ts'
 import { routes } from '../routes.ts'
 
 class FooBarApiImpl implements FooBarApi {
-  async getMySeq() {
+  test: (err: string | undefined, __user: any) => Promise<TestResponse> = async err => {
+    if (err)
+      throw new Error(err)
     return {
       status: 200,
       body: {
         value: 1,
         create_time: dateUtil.now().toDate()
       }
-    } satisfies GetMySeqResponse
+    } satisfies TestResponse
   }
 }
 routes.fooBar = new FooBarApiImpl()
