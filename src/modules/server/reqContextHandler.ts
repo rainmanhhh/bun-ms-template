ea27fbda324bf7d2e780ee6ef42bc693a3ff2ad6
huangwl <410488219@qq.com>
2025-07-14 15:38:18 +0800

    feat: reqContextHandler支持res（Response）

--- src/modules/server/reqContextHandler.ts
+++ src/modules/server/reqContextHandler.ts
@@ -1,4 +1,4 @@
-import type { Request } from 'express'
+import type { Request, Response } from 'express'
 import { AsyncLocalStorage } from 'node:async_hooks'
 import { server } from './server.ts'
 
@@ -8,24 +8,21 @@ function getStore() {
   const store = asyncLocalStorage.getStore()
   if (!store)
     throw new Error('current context is not in async hook')
-  return store as { req: Request }
+  return store as { req: Request, res: Response }
 }
 
 export const reqContext = {
   get req() {
     return getStore().req
   },
-  set req(req: Request) {
-    getStore().req = req
-  },
-  runWithReq(req: Request, callback: () => any) {
-    asyncLocalStorage.run({ req }, callback)
-  },
+  get res() {
+    return getStore().res
+  }
 }
 
 export default function () {
   server.use(async (req, res, next) => {
-    reqContext.runWithReq(req, next)
+    asyncLocalStorage.run({ req, res }, next)
   })
 }
 export const order = -1000
