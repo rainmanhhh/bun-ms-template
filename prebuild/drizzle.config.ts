6235e97d5f976941b1790c9d7ccb61a1ce30b2d6
huangwl <410488219@qq.com>
2025-08-08 11:56:39 +0800

    feat: update drizzle config log

--- prebuild/drizzle.config.ts
+++ prebuild/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 import { appConfig } from '../src/config/appConfig'
 
 const dbUrl = appConfig.dbUrl ?? ''
-console.log('dbUrl: ', dbUrl)
+console.log('drizzle generating db schema. dbUrl: ', dbUrl)
 
 export default defineConfig({
   out: './drizzle',
