c8b4b6e32023074a14f7a1cd3863e1598186f23f
huangwl <410488219@qq.com>
2026-03-05 11:35:33 +0800

    feat: 为服务启动脚本增加cwd标识

--- scripts/start.prod.sh
+++ scripts/start.prod.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cd "$(dirname "$0")" || exit
+CWD=$(readlink -f "$(dirname "$0")")
+cd $CWD || exit
 ./end.sh
 
 source /etc/profile.d/bun.sh # set PATH for bun
 
-NODE_ENV=production bun --bun dist/index.js > out.log &
+NODE_ENV=production bun --cwd=$CWD --bun dist/index.js > out.log &
 echo $! > run.pid
 
 PID=$(cat run.pid)
