82bfc3989a33bfa9172fa96312f2500377a6ea2a
huangwl <410488219@qq.com>
2026-03-09 09:50:27 +0800

    docs: add comment for IAppConfig

--- src/config/IAppConfig.d.ts
+++ src/config/IAppConfig.d.ts
@@ -5,13 +5,32 @@ import type { ISAKConfig } from '~/config/ISAKConfig'
 import type { JwtConfig } from '~/util/jwt/JwtUtil.ts'
 
 export interface IAppConfig {
+  /**
+   * 服务名（用于微服务注册中心）
+   */
   name: string
+  /**
+   * 服务描述（说明性字段）
+   */
+  desc?: string
+  /**
+   * web端口配置
+   */
   server: {
     port: number
     failOnUnknownProperties?: boolean
   }
+  /**
+   * 日志配置
+   */
   log: ILogConfig
+  /**
+   * eureka连接配置
+   */
   eureka?: IEurekaConfig
+  /**
+   * 数据库连接配置
+   */
   db?: IDbConfig
   /**
    * 网关地址，必须以斜杠`/`结尾
@@ -21,6 +40,9 @@ export interface IAppConfig {
    * 微服务访问密钥ServiceApiKey配置
    */
   sak?: ISAKConfig
+  /**
+   * json web token配置
+   */
   jwt?: JwtConfig
   // busi: {}
 }
