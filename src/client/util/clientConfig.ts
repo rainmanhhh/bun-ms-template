da21b25341cb25e411a94dd3fd2585cbf93e1cc6
huangwl <410488219@qq.com>
2026-03-18 17:57:38 +0800

    feat: clientConfig用单独logger

--- src/client/util/clientConfig.ts
+++ src/client/util/clientConfig.ts
@@ -1,5 +1,7 @@
 import {appConfig} from '~/config/appConfig.ts'
 import {genServiceApiKey} from '~/client/util/genServiceApiKey.ts'
+import {getLogger} from '~/logger.ts'
+const logger = getLogger('client.util.clientConfig')
 
 type FetchAPI = (url: string, init?: RequestInit) => Promise<Response>
 
@@ -10,6 +12,7 @@ export function clientConfig(serviceName: string) {
       init.headers = new Headers(init.headers)
       const sak = genServiceApiKey()
       init.headers.set(sak.name, sak.value)
+      logger.debug('fetch [%s], init: %o', url, init)
       return fetch(url, init)
     }) satisfies FetchAPI
   }
