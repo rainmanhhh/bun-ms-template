7d015e61e68e22c5098f6efe0caba28f8868bf8a
huangwl <410488219@qq.com>
2026-03-18 15:22:41 +0800

    feat: 支持非object和array的json请求报文体

--- src/modules/server/bodyHandler.ts
+++ src/modules/server/bodyHandler.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { server } from './server.ts'
 
 export default function () {
-  server.use(express.json())
+  server.use(express.json({ strict: false }))
   server.use(express.urlencoded({ extended: true }))
 }
 export const order = -200
